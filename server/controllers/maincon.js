5fc3a3e124893ccf6ec70d583621e86014980a40
sekhar <munisekharudavalapati1@gmail.com>
2024-07-03 16:38:47 +0530

    Your user's password is at risk!!!!

--- server/controllers/maincon.js
+++ server/controllers/maincon.js
@@ -1,13 +1,14 @@
 const squelize=require('../models/signup')
-
-exports.singupformdata=(req,res,next)=>{
+const bcrypt=require('bcrypt');
+exports.singupformdata= async (req,res,next)=>{
     const {name,phone,email,password}=req.body;  
+    const bcryptPassword=await bcrypt.hash(password,10)
     try{
         const databace=squelize.create({
             name:name,
             phone:phone,
             email:email,
-            password:password
+            password:bcryptPassword
         })
         res.status(200).send("Databace ok")
     }catch(err){
@@ -17,11 +18,11 @@ exports.singupformdata=(req,res,next)=>{
 
 exports.loginformdata=async (req,res,next)=>{
     const {email,password}=req.body;
-    
     try{
       const user= await squelize.findOne({where:{email:email}})
       if(user){
-         if(user.password==password){
+        const validPassword=bcrypt.compare(user.password,password)
+         if(validPassword){
             res.status(200).send('login successful')
          }else{
             res.status(200).send('pass word not ok')
