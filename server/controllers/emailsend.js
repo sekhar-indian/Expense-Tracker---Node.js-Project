b3bb5dc3a5247b6452a9a9c8b6d014cfb1957f6b
sekhar <munisekharudavalapati1@gmail.com>
2024-07-07 11:22:43 +0530

    klj

--- server/controllers/emailsend.js
+++ server/controllers/emailsend.js
@@ -1,31 +1,4 @@
-const brevo = require('@getbrevo/brevo');
-var instance = require('sib-api-v3-sdk');
-let defaultClient = instance.ApiClient.instance;
-exports.forget = async (req, res, next) => {
-     
-  let apiKey = defaultClient.authentications['api-key'];
-  apiKey.apiKey = '';
-  
-  let apiInstance = new brevo.TransactionalEmailsApi();
-  let sendSmtpEmail = new brevo.SendSmtpEmail();
-  
-  sendSmtpEmail.subject = "My {{params.subject}}";
-  sendSmtpEmail.htmlContent = "<html><body><h1>Common: This is my first transactional email {{params.parameter}}</h1></body></html>";
-  sendSmtpEmail.sender = { "name": "John", "email": "munisekharudavalapati@gmail.com" };
-  sendSmtpEmail.to = [
-    { "email": "munisekharudavalapati2@gmail.com", "name": "sample-name" }
-  ];
-  sendSmtpEmail.replyTo = { "email": "munisekharudavalapati2@gmail.com", "name": "sample-name" };
-  sendSmtpEmail.headers = { "Some-Custom-Name": "unique-id-234" };
-  sendSmtpEmail.params = { "parameter": "My param value", "subject": "common subject" };
-  
-  
-  apiInstance.sendTransacEmail(sendSmtpEmail).then(function (data) {
-    console.log('API called successfully. Returned data: ' + JSON.stringify(data));
-  }, function (error) {
-    console.error(error);
-  });
-};
+
  
 
 // const bcrypt=require('bcrypt')
